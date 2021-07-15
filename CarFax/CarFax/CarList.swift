d46a242193154534ded74fe81d74115752212dc8
mark bergeson <markbergeson@Marks-MacBook-Pro.local>
2021-07-14 21:39:56 -0600

    Setup Viewcontroller and model object

---
+++ CarFax/CarFax/CarList.swift
@@ -0,0 +1,23 @@
+//
+//  CarList.swift
+//  CarFax
+//
+//  Created by Mark bergeson on 7/14/21.
+//
+
+import Foundation
+
+struct AllCars: Codable {
+    let listings:[AllCars]
+}
+
+struct CarList: Codable {
+    let year: Int
+    let make: String
+    let model: String?
+    let listprice: Int
+    let mileage: Int
+    let city: String
+    let state : String
+    
+}
--- CarFax/CarFax/ViewController.swift
+++ CarFax/CarFax/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+private extension ViewController {
+    
+    func setupView() {
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150.0
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
